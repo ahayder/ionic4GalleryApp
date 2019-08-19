d9bfae8fe846acc57938d1ed3dea92ad51734597
Ali Hayder/SR.Developer/MISAli.hayderWFM/GENEX <ali.hayder@genexinfosys.com>
2019-08-19 17:42:29 +0600

    Working app

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -5,7 +5,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
-  styleUrls: ['tab2.page.scss']
+  styleUrls: ['tab2.page.scss'],
+  providers: [PhotoService]
 })
 export class Tab2Page {
 
